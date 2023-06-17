1fbe7d2af6fc92a0e5c7cad2d5e31f89ddbc2796
Reaien <ivanduarteherrera@gmail.com>
2023-06-17 02:03:02 -0400

    tabla modificada agregadas foreans key y tablas relacionales tipo_producto y categorias

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Producto, Categoria, TipoProducto
 # Register your models here.
 admin.site.register(Producto)
+admin.site.register(Categoria)
+admin.site.register(TipoProducto)
--- tienda/models.py
+++ tienda/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+#tabla foranea categoria
+class Categoria(models.Model):
+    nombre_categoria = models.CharField(max_length=100)
+
+    def __str__(self):
+        texto = "{0}"
+        return texto.format(self.nombre_categoria)
+
+#tabla tipo producto
+class TipoProducto(models.Model):
+    nombre_tipo_producto = models.CharField(max_length=100)
+
+    def __str__(self):
+        texto = "{0}"
+        return texto.format(self.nombre_tipo_producto)
+
+#Tabla producto
 class Producto(models.Model):
     id_producto = models.CharField(primary_key=True, max_length=6)
     nombre_producto = models.CharField(max_length=200)
@@ -11,8 +29,9 @@ class Producto(models.Model):
     editorial = models.CharField(max_length=80)
     idioma =  models.CharField(max_length=70)
     paginas = models.CharField(max_length=4)
-    categoria = models.CharField(max_length=200)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
     sinopsis = models.CharField(max_length=400)
+    tipo_producto = models.ForeignKey(TipoProducto,null=True, on_delete=models.PROTECT)
     imagen = models.ImageField(upload_to="tienda/", null=True, blank=True)
 
     def __str__(self):
